85f4df67304cd9e6825d7c9d4e0ecc3bd13fdc8f
Mahir <mahir.hasan@m.pilinux.me>
2019-03-15 12:01:27 +0100

    Package: database | Initialize and connect to DB - MySQL implemented

---
+++ database/dbConnect.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"fmt"
+	"github.com/GoREST/config"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var DB *gorm.DB
+var err error
+
+func InitDB() *gorm.DB {
+	var db = DB
+
+	configureDB := config.ConfigMain()
+
+	driver := configureDB.Database.DbDriver
+	username := configureDB.Database.DbUser
+	password := configureDB.Database.DbPass
+	database := configureDB.Database.DbName
+	host := configureDB.Database.DbHost
+	port := configureDB.Database.DbPort
+
+	if driver == "mysql" {
+		db, err = gorm.Open(driver, username+":"+password+"@tcp("+host+":"+port+")/"+database+"?charset=utf8&parseTime=True&loc=Local")
+		if err != nil {
+			fmt.Println("DB err: ", err)
+		}
+		// Only for debugging
+		if err == nil {
+			fmt.Println("DB connection successful!")
+		}
+
+	}
+
+	DB = db
+
+	return DB
+}
