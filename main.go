4585a32aca9018cceb5f8183750ed3129a44dca3
Mahir <mahir.hasan@m.pilinux.me>
2019-03-16 21:30:14 +0100

    Package: main | APIv1.0 | main function including router

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	//"fmt"
+
+	"github.com/GoREST/config"
+	"github.com/GoREST/controller"
+	"github.com/GoREST/database"
+	"github.com/GoREST/lib/middleware"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+func main() {
+	configure := config.ConfigMain()
+	db := database.InitDB()
+	defer db.Close()
+
+	// Debugging - environment variables
+	/*
+		fmt.Println(configure.Server.ServerPort)
+		fmt.Println(configure.Database.DbDriver)
+		fmt.Println(configure.Database.DbUser)
+		fmt.Println(configure.Database.DbPass)
+		fmt.Println(configure.Database.DbName)
+		fmt.Println(configure.Database.DbHost)
+		fmt.Println(configure.Database.DbPort)
+	*/
+
+	gin.SetMode(gin.ReleaseMode) // Omit this line to enable debug mode
+	router := gin.Default()
+	router.Use(middleware.CORS())
+
+	// API:v1.0
+	// Non-protected routes
+	v1 := router.Group("/api/v1/")
+	{
+		// User
+		v1.GET("users", controller.GetUsers)
+		v1.GET("users/:id", controller.GetUser)
+		v1.POST("users", controller.CreateUser)
+		v1.PUT("users/:id", controller.UpdateUser)
+		v1.DELETE("users/:id", controller.DeleteUser)
+
+		// Post
+		v1.GET("posts", controller.GetPosts)
+		v1.GET("posts/:id", controller.GetPost)
+		v1.POST("posts", controller.CreatePost)
+		v1.PUT("posts/:id", controller.UpdatePost)
+		v1.DELETE("posts/:id", controller.DeletePost)
+	}
+
+	router.Run(":" + configure.Server.ServerPort)
+
+}
