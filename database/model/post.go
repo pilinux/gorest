b23c1cdc9976e78bd881b8163401c3b63cedb398
mahir <mahir.hasan@m.pilinux.me>
2020-12-20 22:58:32 +0100

    Updated `Post` model
    `User` --> hasMany --> `Post`

--- database/model/post.go
+++ database/model/post.go
@@ -1,12 +1,16 @@
 package model
 
 import (
-	"github.com/jinzhu/gorm"
+	"time"
 )
 
 // Post model - `posts` table
 type Post struct {
-	gorm.Model
-	Title string `json:"Title"`
-	Body  string `json:"Body"`
+	PostID    uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+	Title     string     `json:"Title"`
+	Body      string     `json:"Body"`
+	IDUser    uint
 }
