bde852428b5aa935f04903386c243f7766fbd952
mahir <mahir.hasan@m.pilinux.me>
2020-12-24 21:39:34 +0100

    `Hobby` model added

---
+++ database/model/hobby.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+)
+
+// Hobby model - `hobbies` table
+type Hobby struct {
+	HobbyID   uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+	Hobby     string     `json:"Hobby"`
+	Users     []User     `gorm:"many2many:user_hobbies"`
+}
