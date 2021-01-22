25cb63abcb647f7f49275235b0cab9241d0f80bc
mahir <mahir.hasan@m.pilinux.me>
2021-01-22 23:54:29 +0100

    Validate email

--- controller/auth.go
+++ controller/auth.go
@@ -2,13 +2,18 @@ package controller
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/piLinux/GoREST/database"
 	"github.com/piLinux/GoREST/database/model"
 
 	"github.com/gin-gonic/gin"
 )
+
+var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 // CreateUserAuth - POST /register
 func CreateUserAuth(c *gin.Context) {
@@ -18,8 +23,14 @@ func CreateUserAuth(c *gin.Context) {
 
 	c.ShouldBindJSON(&auth)
 
+	if isEmailValid(auth.Email) == false {
+		createAuth = 1 // invalid email
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
 	if err := db.Where("email = ?", auth.Email).First(&auth).Error; err == nil {
-		createAuth = 1 // email is already registered
+		createAuth = 2 // email is already registered
 		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
@@ -36,4 +47,25 @@ func CreateUserAuth(c *gin.Context) {
 			c.JSON(http.StatusCreated, auth)
 		}
 	}
+}
+
+// isEmailValid checks if the email provided passes the required structure
+// and length test. It also checks the domain has a valid MX record.
+// Credit: Edd Turtle
+func isEmailValid(e string) bool {
+	if len(e) < 3 || len(e) > 254 {
+		return false
+	}
+
+	if !emailRegex.MatchString(e) {
+		return false
+	}
+
+	parts := strings.Split(e, "@")
+	mx, err := net.LookupMX(parts[1])
+	if err != nil || len(mx) == 0 {
+		return false
+	}
+
+	return true
 }
