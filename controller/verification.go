530318b363152f53b0492612f0f41b5271d1fec5
pilinux <mahir.hasan@m.pilinux.me>
2022-08-30 00:12:13 +0200

    removed log entry created from user input

--- controller/verification.go
+++ controller/verification.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"context"
-	"errors"
 	"net/http"
 	"strings"
 	"time"
@@ -68,10 +67,6 @@ func VerifyEmail(c *gin.Context) {
 	if err := client.Do(ctx, radix.FlatCmd(&result, "DEL", data.key)); err != nil {
 		log.WithError(err).Error("error code: 1063")
 	}
-	if result == 0 {
-		err := errors.New("failed to delete key from redis: " + data.key)
-		log.WithError(err).Error("error code: 1064")
-	}
 
 	// update verification status in database
 	db := database.GetDB()
@@ -93,7 +88,7 @@ func VerifyEmail(c *gin.Context) {
 	tx := db.Begin()
 	if err := tx.Save(&auth).Error; err != nil {
 		tx.Rollback()
-		log.WithError(err).Error("error code: 1065")
+		log.WithError(err).Error("error code: 1064")
 		renderer.Render(c, gin.H{"msg": "internal server error"}, http.StatusInternalServerError)
 		return
 	}
