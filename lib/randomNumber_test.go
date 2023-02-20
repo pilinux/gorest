a1b23803118d5b2971f1a19e024cad8d63a55d90
pilinux <mahir.hasan@m.pilinux.me>
2023-02-20 17:07:10 +0100

    test: for `lib.SecureRandomNumber`

---
+++ lib/randomNumber_test.go
@@ -0,0 +1,45 @@
+package lib_test
+
+import (
+	"testing"
+
+	"github.com/pilinux/gorest/lib"
+)
+
+func TestSecureRandomNumber(t *testing.T) {
+	tests := []struct {
+		name       string
+		totalDigit uint64
+		minimum    uint64
+		maximum    uint64
+	}{
+		{
+			name:       "test case 1",
+			totalDigit: 3,
+			minimum:    100,
+			maximum:    999,
+		},
+		{
+			name:       "test case 2",
+			totalDigit: 5,
+			minimum:    10000,
+			maximum:    99999,
+		},
+		{
+			name:       "test case 3",
+			totalDigit: 1,
+			minimum:    0,
+			maximum:    9,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lib.SecureRandomNumber(tt.totalDigit)
+
+			if got < tt.minimum || got > tt.maximum {
+				t.Errorf("SecureRandomNumber() = %v, want in between %v and %v", got, tt.minimum, tt.maximum)
+			}
+		})
+	}
+}
