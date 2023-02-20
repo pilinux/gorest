a116e80b526212076b9b537cc045c18d3e5d4974
pilinux <mahir.hasan@m.pilinux.me>
2023-02-20 17:11:01 +0100

    test: `lib.HashPass`

---
+++ lib/hashing_test.go
@@ -0,0 +1,58 @@
+package lib_test
+
+import (
+	"testing"
+
+	"github.com/pilinux/gorest/lib"
+)
+
+type hashPassTest struct {
+	name         string
+	config       lib.HashPassConfig
+	password     string
+	expectedErr  bool
+	nonExpectedH string
+}
+
+func TestHashPass(t *testing.T) {
+	tests := []hashPassTest{
+		{
+			name: "blank password",
+			config: lib.HashPassConfig{
+				Memory:      32,
+				Iterations:  2,
+				Parallelism: 1,
+				SaltLength:  16,
+				KeyLength:   16,
+			},
+			password:     "",
+			expectedErr:  false,
+			nonExpectedH: "", // empty string
+		},
+		{
+			name: "with password",
+			config: lib.HashPassConfig{
+				Memory:      32,
+				Iterations:  2,
+				Parallelism: 1,
+				SaltLength:  16,
+				KeyLength:   16,
+			},
+			password:     "password123",
+			expectedErr:  false,
+			nonExpectedH: "", // empty string
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			h, err := lib.HashPass(test.config, test.password)
+			if (err != nil) != test.expectedErr {
+				t.Errorf("unexpected error: got %v, want %v", err, test.expectedErr)
+			}
+			if h == test.nonExpectedH {
+				t.Errorf("unexpected hash: got %v, want %v", h, test.nonExpectedH)
+			}
+		})
+	}
+}
