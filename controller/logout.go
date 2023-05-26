d6db49c5dded0bb9612ba3aa5d9161a284dbb543
pilinux <mahir.hasan@m.pilinux.me>
2023-05-26 15:17:33 +0200

    feat: added logout process

---
+++ controller/logout.go
@@ -0,0 +1,65 @@
+package controller
+
+import (
+	"net/http"
+	"reflect"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/pilinux/gorest/config"
+	"github.com/pilinux/gorest/handler"
+	"github.com/pilinux/gorest/lib/renderer"
+)
+
+// Logout -
+// if 'AUTH_COOKIE_ACTIVATE=yes', ddelete tokens from client browser
+// if Redis is enabled, save invalid tokens in Redis up until the expiry time
+// dependency: JWT
+func Logout(c *gin.Context) {
+	// verify that JWT service is enabled in .env
+	if !config.IsJWT() {
+		renderer.Render(c, gin.H{"message": "JWT service not enabled"}, http.StatusNotImplemented)
+		return
+	}
+
+	// app security settings
+	configSecurity := config.GetConfig().Security
+
+	jtiAccess := c.GetString("jtiAccess")
+	expAccess := c.GetInt64("expAccess")
+
+	jtiRefresh := c.GetString("jtiRefresh")
+	expRefresh := c.GetInt64("expRefresh")
+
+	// if cookie is enabled, delete the cookie from client browser
+	if configSecurity.AuthCookieActivate {
+		c.SetSameSite(configSecurity.AuthCookieSameSite)
+		c.SetCookie(
+			"accessJWT",
+			"",
+			-1,
+			configSecurity.AuthCookiePath,
+			configSecurity.AuthCookieDomain,
+			configSecurity.AuthCookieSecure,
+			configSecurity.AuthCookieHTTPOnly,
+		)
+		c.SetCookie(
+			"refreshJWT",
+			"",
+			-1,
+			configSecurity.AuthCookiePath,
+			configSecurity.AuthCookieDomain,
+			configSecurity.AuthCookieSecure,
+			configSecurity.AuthCookieHTTPOnly,
+		)
+	}
+
+	resp, statusCode := handler.Logout(jtiAccess, jtiRefresh, expAccess, expRefresh)
+
+	if reflect.TypeOf(resp.Message).Kind() == reflect.String {
+		renderer.Render(c, resp, statusCode)
+		return
+	}
+
+	renderer.Render(c, resp.Message, statusCode)
+}
---
+++ handler/logout.go
@@ -0,0 +1,76 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/mediocregopher/radix/v4"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/pilinux/gorest/config"
+	"github.com/pilinux/gorest/database"
+	"github.com/pilinux/gorest/database/model"
+)
+
+// Logout handles jobs for controller.Logout
+func Logout(jtiAccess, jtiRefresh string, expAccess, expRefresh int64) (httpResponse model.HTTPResponse, httpStatusCode int) {
+	// Redis not enabled
+	if !config.IsRedis() {
+		httpResponse.Message = "logout successful"
+		httpStatusCode = http.StatusOK
+		return
+	}
+
+	// Redis enabled
+	client := *database.GetRedis()
+	rConnTTL := config.GetConfig().Database.REDIS.Conn.ConnTTL
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(rConnTTL)*time.Second)
+	defer cancel()
+
+	jwtAccess := model.KeyValue{}
+	jwtRefresh := model.KeyValue{}
+
+	jwtAccess.Key = config.PrefixJtiBlacklist + jtiAccess
+	jwtAccess.Value = strconv.FormatInt(expAccess, 10)
+
+	jwtRefresh.Key = config.PrefixJtiBlacklist + jtiRefresh
+	jwtRefresh.Value = strconv.FormatInt(expRefresh, 10)
+
+	// set key in Redis
+	if err := client.Do(ctx, radix.FlatCmd(nil, "SET", jwtAccess.Key, jwtAccess.Value)); err != nil {
+		log.WithError(err).Error("error code: 1016.1")
+		httpResponse.Message = "internal server error"
+		httpStatusCode = http.StatusInternalServerError
+		return
+	}
+
+	// set TTL
+	if err := client.Do(ctx, radix.FlatCmd(nil, "EXPIREAT", jwtAccess.Key, expAccess)); err != nil {
+		log.WithError(err).Error("error code: 1016.2")
+		httpResponse.Message = "internal server error"
+		httpStatusCode = http.StatusInternalServerError
+		return
+	}
+
+	// set key in Redis
+	if err := client.Do(ctx, radix.FlatCmd(nil, "SET", jwtRefresh.Key, jwtRefresh.Value)); err != nil {
+		log.WithError(err).Error("error code: 1016.3")
+		httpResponse.Message = "internal server error"
+		httpStatusCode = http.StatusInternalServerError
+		return
+	}
+
+	// set TTL
+	if err := client.Do(ctx, radix.FlatCmd(nil, "EXPIREAT", jwtRefresh.Key, expRefresh)); err != nil {
+		log.WithError(err).Error("error code: 1016.4")
+		httpResponse.Message = "internal server error"
+		httpStatusCode = http.StatusInternalServerError
+		return
+	}
+
+	httpResponse.Message = "logout successful"
+	httpStatusCode = http.StatusOK
+	return
+}
