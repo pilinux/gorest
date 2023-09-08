ec68bb42fe02d8a9e3b7e71a5093dcbc489f1e8a
pilinux <mahir.hasan@m.pilinux.me>
2023-09-08 16:20:00 +0200

    GetUserByEmail - support both plaintext and cipher

--- service/auth.go
+++ service/auth.go
@@ -7,6 +7,7 @@ import (
 
 	"golang.org/x/crypto/blake2b"
 
+	"github.com/pilinux/gorest/config"
 	"github.com/pilinux/gorest/database"
 	"github.com/pilinux/gorest/database/model"
 )
@@ -14,14 +15,33 @@ import (
 // GetUserByEmail ...
 func GetUserByEmail(email string) (*model.Auth, error) {
 	db := database.GetDB()
+	var err error
 
 	var auth model.Auth
 
-	if err := db.Where("email = ? ", email).First(&auth).Error; err != nil {
-		return nil, err
+	// when email is saved in plaintext
+	if err = db.Where("email = ? ", email).First(&auth).Error; err == nil {
+		return &auth, nil
 	}
 
-	return &auth, nil
+	// encryption at rest
+	if config.IsCipher() {
+		// hash of the email in hexadecimal string format
+		emailHash, err := CalcEmailHash(
+			email,
+			config.GetConfig().Security.Blake2bSec,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		// email must be unique
+		if err = db.Where("email_hash = ?", emailHash).First(&auth).Error; err == nil {
+			return &auth, nil
+		}
+	}
+
+	return nil, err
 }
 
 // CalcEmailHash generates a fixed-sized BLAKE2b-256 hash of the email
