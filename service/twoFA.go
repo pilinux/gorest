307777dcae8f855c1f7789b2b4212d28e2790e84
pilinux <mahir.hasan@m.pilinux.me>
2023-09-10 16:15:52 +0200

    function to generate random alphanumeric code

--- service/twoFA.go
+++ service/twoFA.go
@@ -1,7 +1,9 @@
 package service
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
+	"math/big"
 
 	"github.com/pilinux/gorest/config"
 )
@@ -22,4 +24,32 @@ func GetHash(dataIn []byte) (dataOut []byte, err error) {
 	)
 
 	return
+}
+
+// RandomByte returns a secure random byte slice of the given length
+func RandomByte(length int) ([]byte, error) {
+	b := make([]byte, length)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// GenerateCode generates a random alphanumeric code of the given length
+func GenerateCode(length int) (string, error) {
+	const characters string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	var code []byte
+	maxIndex := big.NewInt(int64(len(characters)))
+
+	for i := 0; i < length; i++ {
+		randomIndex, err := rand.Int(rand.Reader, maxIndex)
+		if err != nil {
+			return "", err
+		}
+		code = append(code, characters[randomIndex.Int64()])
+	}
+
+	return string(code), nil
 }
