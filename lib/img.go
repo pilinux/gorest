486ce32ff713d642c9cc3fc013bf251c94fa0992
pilinux <mahir.hasan@m.pilinux.me>
2025-04-20 12:59:24 +0200

    lib: ByteToPNG - change param name to avoid confusion
    `path` -> `dir`

--- lib/img.go
+++ lib/img.go
@@ -11,17 +11,17 @@ import (
 )
 
 // ByteToPNG - generate PNG from bytes and save on the disk
-func ByteToPNG(imgByte []byte, path string) (string, error) {
+func ByteToPNG(imgByte []byte, dir string) (string, error) {
 	img, _, err := image.Decode(bytes.NewReader(imgByte))
 	if err != nil {
 		return "", err
 	}
 
 	newImg := "2fa-" + uuid.NewString() + ".png"
-	fullPath := filepath.Join(path, newImg)
+	fullPath := filepath.Join(dir, newImg)
 
 	// prevent directory traversal attacks by validating the path
-	fullPath, err = ValidatePath(fullPath, path)
+	fullPath, err = ValidatePath(fullPath, dir)
 	if err != nil {
 		return "", err
 	}
