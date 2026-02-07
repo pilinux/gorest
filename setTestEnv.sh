a6da81f57b76a37fd3204204eb139880bd89b922
pilinux <mahir.hasan@m.pilinux.me>
2026-02-07 22:34:08 +0100

    chore: for testing on local machine

---
+++ setTestEnv.sh
@@ -0,0 +1,4 @@
+export TEST_ENV_URL="https://s3.nl-ams.scw.cloud/ci.config/github.action/gorest.pilinux/.env"
+export TEST_INDEX_HTML_URL="https://s3.nl-ams.scw.cloud/ci.config/github.action/gorest.pilinux/index.html"
+export TEST_KEY_FILE_LOCATION="https://s3.nl-ams.scw.cloud/ci.config/github.action/gorest.pilinux"
+export TEST_SENTRY_DSN="https://use-your-own-sentry-dsn"
